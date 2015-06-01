b6e1de03b75806317423665f2ca396fb49ecd006
FasnaJF <jffasna@gmail.com>
2015-06-01 08:24:00 +0530

    Redirect changes 01-06-2015 08:24 AM

--- application/controllers/Verify.php
+++ application/controllers/Verify.php
@@ -16,21 +16,35 @@ class Verify extends CI_Controller{
         $data = array();
         $this->load->model('user');
         $tempuser = $this->user->getTempUser($verifyCode);
+        //print_r ($tempuser);
 
-        foreach ($tempuser as $row)
-        {
-            $data['Email'] = $row['Email'];
-            $data['Password'] = $row['Password'];
-            $data['Flag'] = $row['Flag'];
-            $id=$this->user->addNewUser($data);
+       $length = sizeof($tempuser);
+        //$code =  ($tempuser[0]['VerifyCode']);
+       // echo $code;
 
+        if($length > 0){
+
+            $this->phpAlert("Your email address verified successfully...");
+
+            foreach ($tempuser as $row)
+            {
+                $data['Email'] = $row['Email'];
+                $data['Password'] = $row['Password'];
+                $data['Flag'] = $row['Flag'];
+                $id=$this->user->addNewUser($data);
+
+            }
+            $this->user->deleteTempUser($verifyCode);
+
+
+            $this->load->helper('url');
+            redirect('https://localhost/MicroPayment/Admin','refresh');
         }
-        $this->user->deleteTempUser($verifyCode);
-
-        $this->phpAlert("Your email address verified successfully...");
-        $this->load->helper('url');
-        redirect('https://localhost/MicroPayment/Admin');
-       // redirect('https://www.google.com');
+        else{
+            $this->phpAlert("You entered a Expired or Invalid URL...");
+            $this->load->helper('url');
+            redirect('https://localhost/MicroPayment/Admin','refresh');
+        }
 
     }
 
